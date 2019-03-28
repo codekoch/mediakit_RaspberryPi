c3163590f2ac2589b69eb3b9476290e2d38c40e1
codekoch <olaf.koch@aeg-kaarst.org>
2019-03-28 15:03:50 +0100

    RaspberryPi

--- install/sbin/restoreMkProfile.sh
+++ install/sbin/restoreMkProfile.sh
@@ -6,29 +6,36 @@ fi
 if [ $dataDevice = "mmcblk0p2" ]; then
     sudo  sed -i 's/datadev=sda2/datadev=mmcblk0p2/g' /sbin/cmdline.txt 
 fi
-exit
+
+if ! [ -e /a ]
+  then
+  sudo mkdir /a
+fi
+
+if ! [ -e /b ]
+  then
+  sudo mkdir /b
+fi
 
 if [ -e /dev/mmcblk0p2 ]
   then
-    echo "SD Filesystem exists." >>  /home/pi/start.log
-    sudo mount /dev/$dataDevice /tmp
-    sudo mount /dev/mmcblk0p1 /media/mk/boot
-    sudo cp /sbin/cmdline.txt /media/mk/boot/
-    sudo cp /sbin/config.txt /media/mk/boot/
+    sudo mount /dev/$dataDevice /b
+    sudo mount /dev/mmcblk0p1 /a
+    sudo cp /sbin/cmdline.txt /a
+    sudo cp /sbin/config.txt /a
 #    sudo /sbin/checkupdate.sh
-    sudo rm -R /tmp/data/Mediakit.img256/home/mk/* #Remove the mk folder.
-    sudo rm -R /tmp/data/Mediakit.img256/home/mk/.* #Remove the mk folder.
+    sudo rm -R /b/data/Mediakit.img256/home/mk/* #Remove the mk folder.
+    sudo rm -R /b/data/Mediakit.img256/home/mk/.* #Remove the mk folder.
     sudo umount /dev/$dataDevice
     sudo umount /dev/mmcblk0p1
 else
-    echo "HD Filesystem exists." >>  /home/pi/start.log
-    sudo mount /dev/$dataDevice /tmp
-    sudo mount /dev/sda1 /media/mk/boot
-    sudo cp /sbin/cmdline.txt /media/mk/boot/ 
-    sudo cp /sbin/config.txt /media/mk/boot/
+    sudo mount /dev/$dataDevice /b
+    sudo mount /dev/sda1 /a
+    sudo cp /sbin/cmdline.txt /a 
+    sudo cp /sbin/config.txt /a
 #    sudo /sbin/checkupdate.sh
-    sudo rm -R /tmp/data/Mediakit.img256/home/mk/* #Remove the mk folder.
-    sudo rm -R /tmp/data/Mediakit.img256/home/mk/.* #Remove the mk folder.
+    sudo rm -R /b/data/Mediakit.img256/home/mk/* #Remove the mk folder.
+    sudo rm -R /b/data/Mediakit.img256/home/mk/.* #Remove the mk folder.
     sudo umount /dev/$dataDevice
     sudo umount /dev/sda1
 fi
