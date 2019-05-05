45c14d2833184449ab2a469f223fc5b6c33fb6ab
codekoch <olaf.koch@aeg-kaarst.org>
2019-05-05 16:49:08 +0100

    start.sh bugs

--- sources/etc/init.d/start.sh
+++ sources/etc/init.d/start.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
 ### wait for all network functions
 check=0
+timestart=$(date +%s)
 while [ $check -lt  3 ]
 do
+timenow=$(date +%s)
+timediff=$(($timenow - $timestart))
+if [ $timediff -gt 90 ]; then
+   zenity --width=400 --height=100 --info --text="Got problems setting up dhcpcd, hostapd or dnsmasq.(90s and no sucess)\nRestart recommended!" --timeout=3 2> /dev/null
+   break
+fi
+
 active1=`ps -aux | grep -i "dhcpcd" | wc -l`
 if [ $active1 -lt 2 ]; then
 sudo service dhcpcd restart
+sleep 3
 fi
 active2=`ps -aux | grep -i "hostapd" | wc -l`
 if [ $active2 -lt 2 ]; then
 sudo service hostapd restart
+sleep 3
 fi
 active3=`ps -aux | grep -i "dnsmasq" | wc -l`
 if [ $active3 -lt 2 ]; then
 sudo service dnsmasq restart
+sleep 3
 fi
 check=$(($active1+$active2+$active3-3))
 done
