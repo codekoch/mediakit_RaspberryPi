afefb6c85d24c569b069aef4d4922f304eba147c
codekoch <45600398+codekoch@users.noreply.github.com>
2019-10-16 15:07:18 +0100

    Update stopMkServer.sh

--- sources/sbin/stopMkServer.sh
+++ sources/sbin/stopMkServer.sh
@@ -4,3 +4,9 @@
 /usr/bin/killall -KILL startFileBrowser.sh
 /usr/bin/killall -KILL startWorkspace.sh
 /usr/bin/killall -KILL node
+/usr/bin/pkill startnode
+/usr/bin/pkill gpicview
+/usr/bin/pkill startFileBrowse
+/usr/bin/pkill startWorkspace
+/usr/bin/pkill node
+
